e18784410b407c9c66dbdc9f9686beae8f2bc000
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-05-12 16:02:54 -0400

    Generate token list

--- python/verdict_tokenizer.py
+++ python/verdict_tokenizer.py
@@ -1,14 +1,14 @@
 import urllib.request
 import re
 
-
+# grab sample text
 url = ("https://raw.githubusercontent.com/rasbt/"
        "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
        "the-verdict.txt")
 file_path = "the-verdict.txt"
 urllib.request.urlretrieve(url,file_path)
 
-
+# preprocess text
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
@@ -17,4 +17,15 @@ print (len(preprocessed))
 
 print(preprocessed[:30])
 
+#sort and output size
+all_words = sorted(set(preprocessed))
+vocab_size = len(all_words)
+print(vocab_size)
+
+#generate token list
+vocab = {token:integer for integer,token in enumerate(all_words)}
+for i, item in enumerate(vocab.items()):
+    print(item)
+    if i >= 50:
+        break
 
