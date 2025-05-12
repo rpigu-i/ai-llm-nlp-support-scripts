484f8a6dc2f071785df31fd94781cbdabdce91e3
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-05-12 16:25:19 -0400

    Add a class based approach to toeknizing text

---
+++ build_a_large_llm_book/simple_tokenizer_v1.py
@@ -0,0 +1,62 @@
+
+import urllib.request
+import re
+
+class GenerateVocab:
+    """ take the sample text and generate a vocab """
+    vocab = {}
+    
+    def __init__(self):
+        url = ("https://raw.githubusercontent.com/rasbt/"
+               "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
+               "the-verdict.txt")
+        file_path = "the-verdict.txt"
+        urllib.request.urlretrieve(url,file_path)
+        
+        # preprocess text
+        with open("the-verdict.txt", "r", encoding="utf-8") as f:
+            raw_text = f.read()
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+         
+        #sort and output size
+        all_words = sorted(set(preprocessed))
+        
+        #generate token list
+        self.vocab = {token:integer for integer,token in enumerate(all_words)}
+ 
+    def get_vocab(self):
+       return self.vocab
+        
+class SimpleTokenizerV1:
+    """ Simple Text Tokenizer """
+
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text 
+
+vocab = GenerateVocab()
+vocab = vocab.get_vocab()
+tokenizer = SimpleTokenizerV1(vocab)
+text = """It's the last thing he painted, you know,"
+       Mrs. Gisburn said with pardonable pride."""
+ids = tokenizer.encode(text)
+print(ids) 
+
+print(tokenizer.decode(ids))
+
