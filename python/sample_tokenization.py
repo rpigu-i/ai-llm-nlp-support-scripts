ee8cc8e880952f5af9583ef2b50020be3586fd6f
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-05-12 13:21:47 -0400

    Update tokenizer to handle for chars

--- python/sample_tokenization.py
+++ python/sample_tokenization.py
@@ -1,7 +1,7 @@
 # sample script to show how to tokenize a string
 
 import re
-text = "Hello, world. This, is a test."
-result = re.split(r'([,.]|\s)', text)
-result = [item for item in result if item.strip()]
+text = "Hello, world. Is this-- a test."
+result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+result = [item.strip() for item in result if item.strip()]
 print (result)
