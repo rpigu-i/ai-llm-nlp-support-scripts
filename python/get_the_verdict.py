412ea424361316873aa3ab8a59f14eb70b0e0193
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-05-12 13:09:07 -0400

    Add download script and sample text

---
+++ python/get_the_verdict.py
@@ -0,0 +1,8 @@
+import urllib.request
+
+# script to grab sample text for LLM experiments
+url = ("https://raw.githubusercontent.com/rasbt/"
+       "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
+       "the-verdict.txt")
+file_path = "the-verdict.txt"
+urllib.request.urlretrieve(url,file_path)
