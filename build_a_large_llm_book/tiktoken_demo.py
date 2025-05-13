abd0f4ae147cacc0f118f0b6a8278fcc09e4928e
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-05-13 13:46:46 -0400

    Add in decode example

--- build_a_large_llm_book/tiktoken_demo.py
+++ build_a_large_llm_book/tiktoken_demo.py
@@ -8,7 +8,10 @@ tokenizer = tiktoken.get_encoding("gpt2")
 
 text = (
     "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
-    "of someunknownPlace."
+    " of someunknownPlace."
 )
 integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
 print (integers)     
+
+strings = tokenizer.decode(integers)
+print(strings)
