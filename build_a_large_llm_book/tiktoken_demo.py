f3d35c0ccd59d21d1a1678f64552a8478f550f0f
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-05-13 13:41:14 -0400

    Add tiktoken demo

---
+++ build_a_large_llm_book/tiktoken_demo.py
@@ -0,0 +1,14 @@
+# make sure to pip install tiktoken
+
+from importlib.metadata import version
+import tiktoken
+print("tiktoken version:", version("tiktoken"))
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
+    "of someunknownPlace."
+)
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print (integers)     
