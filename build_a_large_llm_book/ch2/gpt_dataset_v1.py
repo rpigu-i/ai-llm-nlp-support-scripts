bbfdca3704c3ac439e2eb14633c00f31749527cc
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-05-14 08:49:47 -0400

    Add example of using gpt dataset

---
+++ build_a_large_llm_book/ch2/gpt_dataset_v1.py
@@ -0,0 +1,12 @@
+import torch
+
+from torch.utils.data import Dataset, DataLoader
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)
+
+
