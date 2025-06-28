db0a6144da5829b67beaef9b05d98842afc677b9
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-06-28 12:51:25 -0400

    Add in pytorch example

--- build_a_large_llm_book/ch2/gpt_dataset_v1.py
+++ build_a_large_llm_book/ch2/gpt_dataset_v1.py
@@ -12,7 +12,7 @@ class GPTDatasetV1(Dataset):
 
         token_ids = tokenizer.encode(txt)
 
-        for i in range(0, len(token_ids)) - max_length, stride):
+        for i in range(0, len(token_ids) - max_length, stride):
             input_chunk = token_ids[i:i + max_length]
             target_chunk = token_ids[i + 1: i + max_length + 1]
             self.input_ids.append(torch.tensor(input_chunk))
@@ -24,3 +24,27 @@ class GPTDatasetV1(Dataset):
     def __getitem__(self, idx):
         return self.input_ids[idx], self.target_ids[idx]
 
+
+    def create_dataloader_v1(txt, batch_size=4, max_length=256,
+                             stride=128, shuffle=True, drop_last=True,
+                             num_workers=0):
+        tokenizer = tiktoken.get_encoding("gpt2")
+        dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+        dataloader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            drop_last=drop_last,
+            num_workers=num_workers
+        )
+        return dataloader
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+dataloader = create_dataloader_v1(
+    raw_text, batch_soze=1, max_length=4, stide=1, shuffle=False)
+
+data_iter = iter(dataloader)
+first_batch = next(data_oter)
+print(first_batch) 
