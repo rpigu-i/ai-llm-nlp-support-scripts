c82d59fac3be5175b077017cf3c71caf293f23a9
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-07-08 08:48:26 -0400

    Add autograd example from appendix A

---
+++ build_a_large_llm_book/appendix_a/pytorch_autograd_examples.py
@@ -0,0 +1,18 @@
+# Examples using PyTorch automatic differentiation engine (autograd)
+
+import torch.nn.functional as F
+import torch
+
+# Logistic regression forward pass (prediction step)
+# Simple logistic regression classifier = single-layer Neural Network
+
+y = torch.tensor([1.0])
+x1 = torch.tensor([1.1])
+w1 = torch.tensor([2.2])
+b = torch.tensor([0.0])
+z = x1 * w1 + b
+a = torch.sigmoid(z)
+loss = F.binary_cross_entropy(a, y)
+
+print ("Loss is:")
+print (loss)
