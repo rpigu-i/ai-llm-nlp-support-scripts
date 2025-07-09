c0f5cf9a0a6d694a4b0070ebd15fea21e7faf461
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-07-09 07:04:21 -0400

    Add in examples of multilayer perception

---
+++ build_a_large_llm_book/appendix_a/pytorch_multilayer_perception.py
@@ -0,0 +1,45 @@
+import torch
+
+class NeuralNetwork(torch.nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super().__init__()
+
+        self.layers = torch.nn.Sequential(
+
+            # 1st hidden layer
+            torch.nn.Linear(num_inputs, 30),
+            torch.nn.ReLU(),
+
+            # 2nd hidden layer
+            torch.nn.Linear(30, 20),
+            torch.nn.ReLU(),
+
+            # output layer
+            torch.nn.Linear(20, num_outputs),
+        )
+
+    def forward(self, x):
+        logits = self.layers(x)
+        return logits
+
+model = NeuralNetwork(50, 3)
+
+print (model)
+
+num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print ("Total number of trainable model parameters:", num_params)
+
+print ("First linear layer:")
+print (model.layers[0].weight)
+
+print ("Print dimensions")
+print (model.layers[0].weight.shape)
+
+print ("Bias")
+print (model.layers[0].bias.shape)
+
+
+
+
+
+
