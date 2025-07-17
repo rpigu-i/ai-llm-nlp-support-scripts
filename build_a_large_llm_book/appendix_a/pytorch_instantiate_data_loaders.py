d8d64f5ee91fe882f2cb48c48c4f6506740c9e06
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-07-17 08:02:15 -0400

    Add data loader example

---
+++ build_a_large_llm_book/appendix_a/pytorch_instantiate_data_loaders.py
@@ -0,0 +1,58 @@
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+
+x_train = torch.tensor([
+    [-1.2, 3.1],
+    [-0.9, 2.9],
+    [-0.5, 2.6],
+    [2.3, -1.1],
+    [2.7, -1.5]
+])
+
+y_train = torch.tensor([0, 0, 0, 1, 1])
+
+x_test = torch.tensor([
+    [-0.8, 2.8],
+    [2.6, -1.6]
+])
+
+y_test = torch.tensor([0,1])
+
+torch.manual_seed(123)
+
+class ToyDataset(Dataset):
+    def __init__(self, x, y):
+        self.features = x
+        self.labels = y
+
+    def __getitem__(self, index):
+        one_x = self.features[index]
+        one_y = self.labels[index]
+        return one_x, one_y
+
+    def __len__(self):
+        return self.labels.shape[0]
+
+train_ds = ToyDataset(x_train, y_train)
+test_ds = ToyDataset(x_test, y_test)
+
+
+train_loader = DataLoader(
+    dataset=train_ds,
+    batch_size=2,
+    shuffle=True,
+    num_workers=0
+)
+
+test_laoder = DataLoader(
+    dataset=test_ds,
+    batch_size=2,
+    shuffle=False,
+    num_workers=0
+)
+
+for idx, (x, y) in enumerate(train_loader):
+    print(f"Batch {idx+1}:", x, y)
+
+
