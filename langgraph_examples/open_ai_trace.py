d20df8eec216aad147048393612b7dc39a7a68e1
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-07-30 08:03:31 -0400

    Simple tracing example using LangGraph

---
+++ langgraph_examples/open_ai_trace.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+from langsmith.wrappers import wrap_openai
+
+openai_client = wrap_openai(OpenAI())
+
+# This is the retriever we will use in RAG
+# This is mocked out, but it could be anything we want
+def retriever(query: str):
+    results = ["Harrison worked at Kensho"]
+    return results
+
+# This is the end-to-end RAG chain.
+# It does a retrieval step then calls OpenAI
+def rag(question):
+    docs = retriever(question)
+    system_message = """Answer the users question using only the provided information below:
+    
+    {docs}""".format(docs="\n".join(docs))
+    
+    return openai_client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": question},
+        ],
+        model="gpt-4o-mini",
+    )
+
+rag("where did harrison work")
