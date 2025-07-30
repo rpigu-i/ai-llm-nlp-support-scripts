49fdc5f012eaad244fa70b5ed7e95edd636dffaf
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-07-30 08:35:46 -0400

    Example with batch size of 8

--- build_a_large_llm_book/ch2/gpt_dataset_v1.py
+++ build_a_large_llm_book/ch2/gpt_dataset_v1.py
@@ -45,10 +45,28 @@ with open("the-verdict.txt", "r", encoding="utf-8") as f:
 dataloader = create_dataloader_v1(
     raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
 
+print ("Demonstrating first batch with a batch size of 1")
 data_iter = iter(dataloader)
 first_batch = next(data_iter)
 print (first_batch)
 
+print ("Demonstrating second batch with a batch size of 1")
 second_batch = next(data_iter)
 print (second_batch)
+
+print ("Demonstrating batch size of 8")
+
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=4, stride=4,
+    shuffle=False
+)
+
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+print ("Inputs:\n", inputs)
+print ("\nTargets:\n", targets)
+
+
+
+
 
