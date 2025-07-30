c24e66a5ea14cd11fb73580816e08b1d1ed931c9
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-07-30 08:15:56 -0400

    Add example of remembering a message

--- langchain_examples/open_ai_chatbot.py
+++ langchain_examples/open_ai_chatbot.py
@@ -1,13 +1,28 @@
+# remember to install langgraph in addition to langchain e.g. pip install -U langgraph
+
 import getpass
 import os
+from langchain_openai import ChatOpenAI
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage
 from langchain_core.messages import AIMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
 
 if not os.environ.get("OPENAI_API_KEY"):
   os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
 
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+# Define the function that calls the model
+def call_model(state: MessagesState):
+    response = model.invoke(state["messages"])
+    return {"messages": response}
+
+
+print ("Simple Open AI Chat example with LangGraph")
+llm = ChatOpenAI()
+llm.invoke("Hello, world!")
 
 print ("Human Message Example")
 response = model.invoke([HumanMessage(content="Hi! I'm Bob")])
@@ -26,3 +41,30 @@ response = model.invoke(
     ]
 )
 print (response.content)
+
+print ("Example using LangGraph")
+
+# Define a new graph
+workflow = StateGraph(state_schema=MessagesState)
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+# Add memory
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+
+# Include a thread to denote a unqiue conversation 
+config = {"configurable": {"thread_id": "abc123"}}
+
+query = "Hi! I'm Bob."
+
+input_messages = [HumanMessage(query)]
+output = app.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print() #output contains all messages in states
+
+
+
+
+
+
+
