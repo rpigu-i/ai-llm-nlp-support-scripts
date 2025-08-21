47d18d75af655319a764e20287449165acafeb09
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-08-21 07:21:39 -0400

    Preprocess example

--- langgraph_examples/preprocess_docs_example.py
+++ langgraph_examples/preprocess_docs_example.py
@@ -2,6 +2,9 @@ import getpass
 import os
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import OpenAIEmbeddings
+from langchain.tools.retriever import create_retriever_tool
 
 def _set_env(key: str):
     if key not in os.environ:
@@ -31,3 +34,16 @@ doc_splits[0].page_content.strip()
 
 print ("Example of split documents")
 print (doc_splits[0])
+
+vectorstore = InMemoryVectorStore.from_documents(
+    documents=doc_splits, embedding=OpenAIEmbeddings()
+)
+retriever = vectorstore.as_retriever()
+
+retriever_tool = create_retriever_tool(
+    retriever,
+    "retrieve_blog_posts",
+    "Search and return information about Lilian Weng blog posts.",
+)
+
+retriever_tool.invoke({"query": "types of reward hacking"})
