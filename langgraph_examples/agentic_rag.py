d05ea671d09bdac7a00187dc0139979d207f8e87
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-08-21 07:21:53 -0400

    Start adding in an Agentic RAG example with langgraph

---
+++ langgraph_examples/agentic_rag.py
@@ -0,0 +1,71 @@
+import getpass
+import os
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import OpenAIEmbeddings
+from langchain.tools.retriever import create_retriever_tool
+from langgraph.graph import MessagesState
+from langchain.chat_models import init_chat_model
+
+def generate_query_or_respond(state: MessagesState):
+    """Call the model to generate a response based on the current state. Given
+    the question, it will decide to retrieve using the retriever tool, or simply respond to the user.
+    """
+    response = (
+        response_model
+        .bind_tools([retriever_tool]).invoke(state["messages"])
+    )
+    return {"messages": [response]}
+
+
+def _set_env(key: str):
+    if key not in os.environ:
+        os.environ[key] = getpass.getpass(f"{key}:")
+
+_set_env("OPENAI_API_KEY")
+
+response_model = init_chat_model("openai:gpt-4.1", temperature=0)
+
+urls = [
+    "https://lilianweng.github.io/posts/2024-11-28-reward-hacking/",
+    "https://lilianweng.github.io/posts/2024-07-07-hallucination/",
+    "https://lilianweng.github.io/posts/2024-04-12-diffusion-video/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+
+
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=100, chunk_overlap=50
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+
+vectorstore = InMemoryVectorStore.from_documents(
+    documents=doc_splits, embedding=OpenAIEmbeddings()
+)
+retriever = vectorstore.as_retriever()
+
+retriever_tool = create_retriever_tool(
+    retriever,
+    "retrieve_blog_posts",
+    "Search and return information about Lilian Weng blog posts.",
+)
+
+retriever_tool.invoke({"query": "types of reward hacking"})
+
+input = {"messages": [{"role": "user", "content": "hello!"}]}
+generate_query_or_respond(input)["messages"][-1].pretty_print()
+
+input = {
+    "messages": [
+        {
+            "role": "user",
+            "content": "What does Lilian Weng say about types of reward hacking?",
+        }
+    ]
+}
+generate_query_or_respond(input)["messages"][-1].pretty_print()
